aac6a1168c4e70286e9ec122e7beba90c06c4cf7
LeviPrado <levioliveira.prado@gmail.com>
2026-07-20 23:24:45 -0300

    A logica é bem especifica!! envolve resto de divisão.

---
+++ 1177.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int T, N[1000];
+    scanf("%d", &T);
+
+    for(int i = 0; i < 1000; i++){
+        N[i] = i % T;
+    }
+    
+    for(int i = 0; i < 1000; i++){
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+
+    return 0;
+}
