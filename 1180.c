5e45f5af6d4d296e7d89a25a6369d8bf4a3c22d4
LeviPrado <levioliveira.prado@gmail.com>
2026-07-22 00:37:29 -0300

    É para ver se o candidato sabe achar o menor numero e usar um contador!

---
+++ 1180.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int N; 
+    int pos = 0;
+
+    scanf("%d", &N);
+    
+    int X[N];
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &X[i]);
+    }
+
+    int menor = X[0];
+
+    for(int i = 1; i < N; i++){
+        if(X[i] < menor){
+            menor = X[i];
+            pos = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", pos);
+
+    return 0;
+}
