352270f141ec672c77581fa2d9291af41c59bd50
LeviPrado <levioliveira.prado@gmail.com>
2026-07-22 01:27:22 -0300

    soma ou tira a media de todos os elementos acima da diagonal principal

---
+++ 1183.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    double M[12][12];
+    double soma = 0;
+    int contador = 0;
+    char C;
+    scanf(" %c", &C); 
+
+    // Lê a Matriz
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+    
+    // implemanta a logica principal do programa
+    for(int i = 0; i < 12; i++){
+        for(int j = i + 1; j < 12; j++){
+            soma += M[i][j];
+            contador++;
+        }
+    }
+
+    // Faz a soma ou a media
+    if(C == 'S'){
+        printf("%.1lf\n", soma);
+    } else if(C == 'M'){
+        printf("%.1lf\n", soma / contador);
+    }
+
+    return 0;
+}
