5f81ef3d0f3e8b1c7ef8e0ab34f9335bafb785fb
LeviPrado <levioliveira.prado@gmail.com>
2026-07-23 00:03:47 -0300

    so mudar a condição do IF

---
+++ 1187.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    double M[12][12];
+    double soma = 0.0;
+    int contador = 0;
+    char C;
+
+    scanf(" %c", &C);
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            if(j > i && j < 11 - i){
+                soma += M[i][j];
+                contador++;
+            }
+        }
+    }
+
+    if(C == 'S'){
+        printf("%.1lf\n", soma);
+    } else if(C =='M'){
+        printf("%.1lf\n", soma / contador);
+    }
+
+    return 0;
+}
