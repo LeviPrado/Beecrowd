c6b617b303f5584d656ee8c52ef132270f7ab2ad
LeviPrado <levioliveira.prado@gmail.com>
2026-07-27 23:58:37 -0300

    logica interessante!

---
+++ 1435.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+
+    int N;
+
+    while(scanf("%d", &N) ==1 && N != 0){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                int cima = i;
+                int esq = j;
+                int baixo = N - 1 - i;
+                int dir = N - 1 - j;
+
+                int menor = cima;
+
+                if (esq < menor){
+                    menor = esq;
+                }
+                if(baixo < menor){
+                    menor = baixo;
+                }
+                if(dir < menor){
+                    menor = dir;
+                }
+                
+                int valor = menor + 1;
+
+                if(j > 0){
+                    printf(" ");
+                }
+                printf("%3d", valor);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
